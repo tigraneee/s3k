81f9e3a7dec54ff7cf8851fb95ff3fa6170c70e9
Xiaotian Jiang <49832003+Sevenqi7@users.noreply.github.com>
2026-05-03 21:26:51 +0200

    uart: fix ti16750 register offset using packed struct (#21)
    * uart: fix ti16750 register offset using packed struct
    
    * Make TI16750 UART register stride configurable
    
    ---------
    
    Co-authored-by: seven7 <734633634@qq.com>

--- projects/ipc/app2/platform/ti16750.c
+++ projects/ipc/app2/platform/ti16750.c
@@ -2,12 +2,67 @@
 
 extern volatile int __uart_base[]; // UART base address
 
+#ifndef UART_REG_STRIDE
+#error "UART_REG_STRIDE must be defined (1 or 4)."
+#endif
+
+#if (UART_REG_STRIDE != 1) && (UART_REG_STRIDE != 4)
+#error "Unsupported UART_REG_STRIDE. Use 1 or 4."
+#endif
+
+struct __attribute__((packed)) uart_reg_t {
+	volatile char val;
+	char __pad[UART_REG_STRIDE - 1];
+};
+
+struct __attribute__((packed)) uart_regs {
+	union {
+		struct uart_reg_t rbr;
+		struct uart_reg_t thr;
+		struct uart_reg_t dll;
+	};
+
+	union {
+		struct uart_reg_t ier;
+		struct uart_reg_t dlm;
+	};
+
+	union {
+		struct uart_reg_t iir;
+		struct uart_reg_t fcr;
+		struct uart_reg_t efr;
+	};
+
+	struct uart_reg_t lcr;
+
+	union {
+		struct uart_reg_t mcr;
+		struct uart_reg_t xon1;
+	};
+
+	union {
+		struct uart_reg_t lsr;
+		struct uart_reg_t xon2;
+	};
+
+	union {
+		struct uart_reg_t msr;
+		struct uart_reg_t xoff1;
+	};
+
+	union {
+		struct uart_reg_t spr;
+		struct uart_reg_t xoff2;
+	};
+};
+
 int __uart_putc(char c, FILE *f)
 {
 	(void)f;
-	while (!(__uart_base[5] & 0x20)) {
+	volatile struct uart_regs *regs = (struct uart_regs *)__uart_base;
+	while (!(regs->lsr.val & 0x20)) {
 	}
-	__uart_base[0] = (unsigned char)c;
+	regs->thr.val = (unsigned char)c;
 	return c;
 }
 
